b87831904bae7e4cb32b1da215d9fd37305982db
Adarsh Dubey <adarshd966@gmail.com>
2026-05-22 00:38:13 +0530

    Inquery feature added for Employee Module (Only static frontend part added and successfully redirect by employee controller)

--- src/main/java/in/sp/main/controller/EmployeeController.java
+++ src/main/java/in/sp/main/controller/EmployeeController.java
@@ -1,3 +1,4 @@
+
 package in.sp.main.controller;
 
 import java.time.LocalDate;
@@ -184,6 +185,17 @@ public class EmployeeController {
 			redirectAttributes.addFlashAttribute("errorMsg","Not able to provide course try After some time...");
 		}
 		return "redirect:/sellCourse";
+	}
+	
+	@GetMapping("/inqueryManagement")
+	public String OpenInqueryManagementPage() {
+		return "inquery-management";
+	}
+	
+	@GetMapping("/employeeLogout")
+	public String OpenLogOutEmployeePage(SessionStatus sessionStatus) {
+		sessionStatus.setComplete();
+		return "employee-login";
 	}
 
 }
